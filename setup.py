5d8142f677de1471946bdaa581b67cfe276f135f
itsme-rk <karthikramesh1813@gmail.com>
2024-12-02 23:27:53 +0530

    env and dependencies installed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Karthik R",
+    author_email="karthikramesh1813@gmail.com",
+    packages=find_packages()
+)
